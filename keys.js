54adb05bf89b1d53a960fff4b2232542f5173398
Jim Gray <JimGray9999@users.noreply.github.com>
2017-07-27 12:54:37 -0400

    initial files

---
+++ keys.js
@@ -0,0 +1,18 @@
+```JavaScript
+console.log('this is loaded');
+
+exports.twitterKeys = {
+  consumer_key: 'H7aZyvnXeTOkPj817E6moQjKL',
+  consumer_secret: 'sKXgqFFdSRcU2e995NLR7q1c2rGRcRS9Pcp1YCPBFFWiHK3emE',
+  access_token_key: '890537862621474816-738ukNRMMqX66fFKe8sC1dKroSrUOxo',
+  access_token_secret: 'FCNtjzGkI9s9U4dcKO2m8FlBiDUbJdsAnYHNqOSBZWG2l',
+}
+
+spotify:
+
+Client ID
+98f61fa98b0c4dd7afeaf4fc261363b6
+Client Secret
+7b3aef64038941749bea1e702fccf07b
+
+```
---
+++ liri.js
@@ -0,0 +1,12 @@
+// Grabs the bands variables
+var keys = require("./keys.js");
+
+// TODO: Liri commands:
+
+/* `my-tweets`
+
+   * `spotify-this-song`
+
+   * `movie-this`
+
+   * `do-what-it-says` */
