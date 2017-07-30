db2c2b7340d6562e47646532c1dc03ae662016f3
Jim Gray <JimGray9999@users.noreply.github.com>
2017-07-29 23:34:45 -0400

    twitter api working, spotify next

--- keys.js
+++ keys.js
@@ -1,22 +1,17 @@
-console.log('this is loaded');
+console.log('keys are loaded...');
 
 exports.twitterKeys = {
-  consumer_key: 'H7aZyvnXeTOkPj817E6moQjKL',
-  consumer_secret: 'sKXgqFFdSRcU2e995NLR7q1c2rGRcRS9Pcp1YCPBFFWiHK3emE',
-  access_token_key: '890537862621474816-738ukNRMMqX66fFKe8sC1dKroSrUOxo',
-  access_token_secret: 'FCNtjzGkI9s9U4dcKO2m8FlBiDUbJdsAnYHNqOSBZWG2l',
+    consumer_key: 'H7aZyvnXeTOkPj817E6moQjKL',
+    consumer_secret: 'sKXgqFFdSRcU2e995NLR7q1c2rGRcRS9Pcp1YCPBFFWiHK3emE',
+    access_token_key: '890537862621474816-738ukNRMMqX66fFKe8sC1dKroSrUOxo',
+    access_token_secret: 'FCNtjzGkI9s9U4dcKO2m8FlBiDUbJdsAnYHNqOSBZWG2l',
 }
 
 exports.spotifyKeys = {
-	/* 
-Client ID
-98f61fa98b0c4dd7afeaf4fc261363b6
-Client Secret
-7b3aef64038941749bea1e702fccf07b */
-
+    id: '98f61fa98b0c4dd7afeaf4fc261363b6',
+    secret: '7b3aef64038941749bea1e702fccf07b'
 }
 
-exports.OMDBAPI = {
-	// API Key here
+exports.omdbKey = {
+    key: "40e9cece"
 }
-
--- liri.js
+++ liri.js
@@ -1,17 +1,64 @@
-// Grabs the API keys
-var keys = require("./keys.js");
+// required node packages
+var keys = require("./keys.js"); // holds API keys...shhhh
+var twitAPI = require("twitter"); // Twitter package
+var Spotify = require('node-spotify-api'); // Spotify package
+var request = require("request"); // request package
+var fs = require("fs");
 
-var command = process.argv[2];
-var searchString = process.argv[3];
 
-// TODO: Liri commands:
+// get the API keys assigned
+var client = new twitAPI({
+    consumer_key: keys.twitterKeys.consumer_key,
+    consumer_secret: keys.twitterKeys.consumer_secret,
+    access_token_key: keys.twitterKeys.access_token_key,
+    access_token_secret: keys.twitterKeys.access_token_secret
+});
+var spotifyAccess = new Spotify({
+    id: keys.spotifyKeys.id,
+    secret: keys.spotifyKeys.secret
+});
 
-if (command === 'my-tweets'){
-	//TODO: display tweets from @MinnieKittyCat
-} else if (command === 'spotify-this-song'){
-	//TODO: get info on song provided
-} else if (command === 'movie-this'){
-	// OMDB the movie entered
-} else if (command === 'do-what-it-says'){
-	//TODO: do what it says....
+// inputs
+var command = process.argv[2]; // command input
+var searchString = process.argv[3]; // search parameter
+
+// Liri commands:
+
+if (command === 'my-tweets') {
+    // parameters for get call to API
+    var params = { screen_name: "MinnieKittyCat", count: 20 };
+
+    // GET call, display last 20 Tweets
+    client.get('statuses/user_timeline', params, function(error, tweets, response) {
+        if (error) throw error;
+
+        console.log("Here are your Minnie Kitty Tweets!");
+        for (i = 0; i < 20; i++) {
+            console.log("----------------------------------------");
+            console.log("Created at: " + tweets[i].created_at);
+            console.log("Tweet text: " + tweets[i].text);
+        }
+    });
+} else if (command === 'spotify-this-song') {
+    // get info on song provided from Spotify API
+
+    // define search parameters
+    // options for type: artist, album or track
+    var songParams = { type: 'track', query: `${searchString}` };
+
+    spotifyAccess.search(songParams, function(error, data) {
+        if (error) { console.log("I am error: " + error); }
+        console.log(data);
+        console.log("Artist(s): ");
+        console.log("Song Name: ");
+        console.log("Preview: ");
+        console.log("Album: ");
+        // https://developer.spotify.com/web-api/search-item/
+    });
+} else if (command === 'movie-this') {
+    // OMDB the movie entered
+} else if (command === 'do-what-it-says') {
+    //TODO: do what it says, read the random.txt file and execute command
+} else {
+    console.log("Invalid command, please try again.")
 }
