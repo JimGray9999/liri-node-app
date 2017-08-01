e16f47959b3cacbad4b228c0ab0abbc57687c855
Jim Gray <JimGray9999@users.noreply.github.com>
2017-07-31 21:31:38 -0400

    update

--- liri.js
+++ liri.js
@@ -28,6 +28,21 @@ var searchString = process.argv[3]; // search parameter
 // Liri commands:
 
 if (command === 'my-tweets') {
+    myTweets();
+} else if (command === 'spotify-this-song') {
+    // get info on song provided from Spotify API
+    spotifyThis();
+    
+    
+} else if (command === 'movie-this') {
+    // TODO: OMDB the movie entered
+} else if (command === 'do-what-it-says') {
+    // TODO: do what it says, read the random.txt file and execute command
+} else {
+    console.log("Invalid command, please try again.")
+}
+
+function myTweets(){
     // parameters for get call to API
     var params = { screen_name: "MinnieKittyCat", count: 20 };
 
@@ -42,9 +57,9 @@ if (command === 'my-tweets') {
             console.log("Tweet text: " + tweets[i].text);
         }
     });
-} else if (command === 'spotify-this-song') {
-    // get info on song provided from Spotify API
+}
 
+function spotifyThis(){
     // define search parameters
     // options for type: artist, album or track
     var songParams = { type: 'track', query: `${searchString}` };
@@ -52,24 +67,34 @@ if (command === 'my-tweets') {
     spotifyAccess.search(songParams, function(error, data) {
         if (error) { console.log("I am error: " + error); }
         
-        console.log(data.tracks.items[0].artists.name);
-        fs.writeFile("testy.json", JSON.stringify(data.tracks), function(err) {
+        // console log testing lines
+        // console.log(data.tracks.items);
+        // console.log(data.tracks.items[0].album.name);
+        // console.log(data.tracks.items[0].name);
+        // console.log(data.tracks.items[0].artists[0].name);  // artist name
+        // console.log(data.tracks.items[0].preview_url);
 
-        // If the code experiences any errors it will log the error to the console.
-        if (err) {
-            return console.log(err);
-        }});
-
-        console.log("Artist(s): ");
-        console.log("Song Name: ");
-        console.log("Preview: ");
-        console.log("Album: ");
+        var albumName = data.tracks.items[1].album.name;
+        var songName = data.tracks.items[1].name;
+        var artistName = data.tracks.items[1].artists[0].name;
+        var previewUrl = data.tracks.items[1].preview_url;
+        
+        if (previewUrl === null){
+            previewUrl = "N/A";
+        }
+        
+        console.log(`Artist(s): ${artistName}`);
+        console.log(`Song Name: ${songName}`);
+        console.log(`Preview: ${previewUrl}`);
+        console.log(`Album: ${albumName}`);
         // https://developer.spotify.com/web-api/search-item/
-    });
-} else if (command === 'movie-this') {
-    // TODO: OMDB the movie entered
-} else if (command === 'do-what-it-says') {
-    // TODO: do what it says, read the random.txt file and execute command
-} else {
-    console.log("Invalid command, please try again.")
+
+        
+        // write results to a JSON file (testing purposes)
+        // fs.writeFile("testy.json", JSON.stringify(data.tracks), function(err) {
+        // // If the code experiences any errors it will log the error to the console.
+        // if (err) {
+        //     return console.log(err);
+        // }});
+        });
 }
